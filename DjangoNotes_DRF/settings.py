a8b99d1587a47f3cb2077fe0f3841db48289cbc3
Harsh Singh <43901843+DankMemes4President@users.noreply.github.com>
2022-06-02 23:42:28 +0530

    heroku stuff

--- DjangoNotes_DRF/settings.py
+++ DjangoNotes_DRF/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY', 'Optional default value')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://mysterious-brushlands-94222.herokuapp.com/]
 
 # Application definition
 
